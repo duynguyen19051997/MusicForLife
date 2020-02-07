54a7a3f7491f6b189c624c236bb14a075c5de48d
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2020-02-07 08:54:29 +0700

    Music For Life

---
+++ src/main/java/duynguyen/musicforlife/MusicforlifeApplication.java
@@ -0,0 +1,13 @@
+package duynguyen.musicforlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicforlifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicforlifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/duynguyen/musicforlife/MusicforlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package duynguyen.musicforlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicforlifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
